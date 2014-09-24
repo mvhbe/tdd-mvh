a21123cdee06cac71bdf85c96506f8319908a154
Guido Van Hoof <guido.van.hoof3@telenet.be>
2014-09-24 21:38:44 +0200

    functional test herschreven met unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
+class BezoekerTest(unittest.TestCase):
 
-assert "Django" in browser.title
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def testWebsiteVisclub(self):
+		# Joske heeft gehoord over de nieuwe site van de visclub.
+		# Hij opent de browser en gaat naar de de site.
+		self.browser.get("http://localhost:8001")
+
+		# De naam van de visclub wordt in de browser titel getoond.
+		self.assertIn('Visclub Moed & Volharding',  self.browser.title)
+
+		# De pagina heeft een titel "Wedstrijden deze maand"
+		self.fail('Finish the test !')
+
+if __name__ == '__main__':
+	unittest.main()
